6e5494cab8567a2c3059ddd6ddef9f9cb8dc2b64
Bida Danylo <dbidyc@gmail.com>
2022-10-21 00:49:54 +0200

    created .gitignore

---
+++ longint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dbidy on 21.10.2022.
+//
+
+#include "longint.h"
---
+++ longint.h
@@ -0,0 +1,14 @@
+//
+// Created by dbidy on 21.10.2022.
+//
+
+#ifndef LAB2_LONGINT_H
+#define LAB2_LONGINT_H
+
+
+class longint {
+
+};
+
+
+#endif //LAB2_LONGINT_H
